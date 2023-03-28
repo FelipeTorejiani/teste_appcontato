a6776abc2a25dde4ac3610ff7bd9157ea26f7b66
Felipe Torejiani <83676740+FelipeTorejiani@users.noreply.github.com>
2023-03-28 17:11:56 -0400

    Arquivo do projeto

---
+++ ProjetoPDS/ProjetoPDS/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace ProjetoPDS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btCadastrarContato = new System.Windows.Forms.Button();
+            this.bt_Listar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(226, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(398, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "MENU DE CADASTRO DE CONTATO";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btCadastrarContato
+            // 
+            this.btCadastrarContato.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btCadastrarContato.Location = new System.Drawing.Point(171, 124);
+            this.btCadastrarContato.Name = "btCadastrarContato";
+            this.btCadastrarContato.Size = new System.Drawing.Size(197, 56);
+            this.btCadastrarContato.TabIndex = 1;
+            this.btCadastrarContato.Text = "CADASTRO CONTATO";
+            this.btCadastrarContato.UseVisualStyleBackColor = true;
+            this.btCadastrarContato.Click += new System.EventHandler(this.btCadastrarContato_Click);
+            // 
+            // bt_Listar
+            // 
+            this.bt_Listar.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_Listar.Location = new System.Drawing.Point(466, 124);
+            this.bt_Listar.Name = "bt_Listar";
+            this.bt_Listar.Size = new System.Drawing.Size(197, 56);
+            this.bt_Listar.TabIndex = 2;
+            this.bt_Listar.Text = "LISTA DE CONTATO";
+            this.bt_Listar.UseVisualStyleBackColor = true;
+            this.bt_Listar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.bt_Listar);
+            this.Controls.Add(this.btCadastrarContato);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btCadastrarContato;
+        private System.Windows.Forms.Button bt_Listar;
+    }
+}
+
---
+++ ProjetoPDS/ProjetoPDS/Formularios/FormListarContato.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using ProjetoPDS.Contexto;
+
+
+
+namespace ProjetoPDS.Formularios
+{
+    public partial class FormListarContato : Form
+    {
+        private MySqlConnection _conexao;
+        public FormListarContato()
+        {
+            InitializeComponent();
+            Conexao();
+            CarregarDados();
+
+        }
+
+        public void Conexao()
+        {
+            string conexaoString = "server=localhost;database=app_contato_bd_tore;user=root;password=root;port=3360";
+            _conexao = new MySqlConnection(conexaoString);
+            _conexao.Open();
+
+        }
+        public void CarregarDados()
+        {
+            var sql = "SELECT * FROM contato ";
+            MySqlCommand cmd = new MySqlCommand(sql, _conexao);
+            MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+
+            DataTable dataTable = new DataTable();
+            da.Fill(dataTable);
+            this.dataGridView1.DataSource = dataTable;
+        }
+
+    }
+}
